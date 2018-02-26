5c8e8829a802af4e30360dd829253e2d53638e25
Seth Walton <sdwalton83@gmail.com>
2018-02-26 12:26:22 -0600

    mongoose connection and routes started on views

---
+++ models/user.js
@@ -0,0 +1,24 @@
+var mongoose 	=	 require('mongoose');
+
+mongoose.connect('mongodb://admin:seth@ds147668.mlab.com:47668/leadershiplearning');
+
+var db 			=	 mongoose.connection;
+
+
+//User Schema
+var UserSchema 	=  mongoose.Schema({
+	username : {
+		type 	: String,
+		index	: true
+	},
+	password :	{
+		type 	: String, 
+		required: true, 
+		bcrypt  : true
+	},
+	email 		: String,
+	name 		: String,
+	profile 	: String
+});
+
+module.exports =  mongoose.model('User', UserSchema);
