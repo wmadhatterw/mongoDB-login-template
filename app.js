948774c173cf9dec16823ad8bdae259078cbdaa2
Seth Walton <sdwalton83@gmail.com>
2018-02-26 12:20:02 -0600

    app typos

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var mongo				=	require('mongodb');
 var mongoose			=	require('mongoose');
 var db 					=	mongoose.connection;
 
-var route 				= 	require('./routes/index');
+var routes 				= 	require('./routes/index');
 var users				=	require('./routes/users');
 
 var app = express();
@@ -70,7 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(flash());
 app.use(function(req,res,next) {
-	res.locals.message = require('express-messages')(req,res)
+	res.locals.messages = require('express-messages')(req,res);
 	next();
 });
 
@@ -82,7 +82,7 @@ app.get('*', function(req,res,next) {
 });
 
 app.use('/', routes);
-app.use('/users',users)
+app.use('/users',users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -94,12 +94,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
+  res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
